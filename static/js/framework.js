94be1645b4a822e3ffa42478f45a4c67f203e9e6
phantamanta44 <evandalong@gmail.com>
2016-04-28 20:06:43 -0400

    Landing page styling

--- static/js/framework.js
+++ static/js/framework.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
+    if (!String.prototype.supplant) {
+        String.prototype.supplant = function (o) {
+            return this.replace(
+                /\{([^{}]*)\}/g,
+                function (a, b) {
+                    var r = o[b];
+                    return typeof r === 'string' || typeof r === 'number' ? r : a;
+                }
+            );
+        };
+    }
+    
     $('a.a-newtab').attr('target', '_BLANK');
+    
+    $('a.navbar-expand').click(function(e) {
+        $('.navbar-links').toggleClass('navbar-expanded');
+    });
 
 });
--- static/js/front.js
+++ static/js/front.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
-    // TODO stuff
-
+    $('div.lp-left-text').each(function(i, o) {
+        var e = $(o);
+        var col = e.attr('data-colour');
+        e.css('background-color', col);
+        e.css('border-top-color', col);
+    });
+    
+    $('div.lp-right-text').each(function(i, o) {
+        var e = $(o);
+        var col = e.attr('data-colour');
+        e.css('background-color', col);
+        e.css('border-bottom-color', col);
+    });
+    
+    $('div.lp-left-img > div, div.lp-right-img > div').each(function(i, o) {
+        var e = $(o);
+        var bg = e.attr('data-bg');
+        e.css('background-image', 'url({bg})'.supplant({bg: bg}));
+    });
+    
+    var years = new Date().getFullYear() - 1998;
+    $('#year-count').text('{years} Years of Excellence'.supplant({years: years}));
+    
 });
