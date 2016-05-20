03d1f90815291e166f1c7420a58e8f289c4a8450
phantamanta44 <evandalong@gmail.com>
2016-05-20 19:14:46 -0400

    Seasons page implementation

--- static/js/page/seasons.js
+++ static/js/page/seasons.js
@@ -1,6 +1,28 @@
 'use strict';
 $(document).ready(function() {
     
-    // TODO stuff
+    var generateBlock = function(y, data) {
+        var elem = $('<div>', {class: 'season-elem elem-4'}).css('background-image', 'url({url})'.supplant({url: data.img}));
+        var sDesc = $('<div>', {class: 'row no-stack season-desc mar-out text-light'});
+        var dName = $('<div>', {class: 'season-name elem-6'});
+        var dYear = $('<div>', {class: 'season-year elem-6 text-right'});
+        
+        dName.html('<h4><small>{name}</small></h4>'.supplant({name: data.name}));
+        dYear.html('<h4><small>{year}</small></h4>'.supplant({year: y}));
+        
+        sDesc.append(dName).append(dYear);
+        return elem.append(sDesc);
+    };
+    
+    var sGrid = $('#season-grid');
+    
+    $.getJSON('static/json/seasons.json', {}, function(r) {
+        var seasonBlocks = [];
+        $.each(r, function(k, v) {
+            seasonBlocks.push(generateBlock(k, v));
+        });
+        for (var i = seasonBlocks.length - 1; i >= 0; i--)
+            sGrid.append(seasonBlocks[i]);
+    });
     
 });
