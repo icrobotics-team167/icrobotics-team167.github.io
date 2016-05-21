f363077fbe59a8c5b2de0ad23eeafa744c1d3afa
phantamanta44 <evandalong@gmail.com>
2016-05-20 23:50:51 -0400

    Some previous season content

--- static/js/page/season.js
+++ static/js/page/season.js
@@ -11,7 +11,10 @@ $(document).ready(function() {
         $.getJSON('static/json/seasons.json', {}, function(r) {
             if (!r[q.y])
                 return fail();
-            console.log(r[q.y]); // TODO Do something with season data
+            var yData = r[q.y];
+            document.title = document.title.replace(/Seasons/, q.y);
+            $('#heading-banner').css('background-image', 'url({url})'.supplant({url: yData.img}));
+            $('#heading-title').html('{year}<small> {name}</small>'.supplant({name: yData.name, year: q.y}));
         });
     }
     else
