28269bc338b53a6f6f8a0e93cd0e1dcc8afc4c95
Reedman20 <swan.gunnarj@gmail.com>
2017-01-25 18:50:06 -0600

    Added sponsors.js and code

---
+++ static/js/page/sponsor.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Programing on 1/25/2017.
+ */
+<div class="piktowrapper-embed" pikto-uid="19631898-new-piktochart" style="height: 300px; position: relative;">
+    <div class="embed-loading-overlay" style="width: 100%; height: 100%; position: absolute; text-align: center;">
+    <img width="60px" alt="Loading..." style="margin-top: 100px" src="https://magic.piktochart.com/loading.gif"/>
+    <p style="margin: 0; padding: 0; font-family: Lato, Helvetica, Arial, sans-serif; font-weight: 600; font-size: 16px">Loading...</p>
+    </div><div class="pikto-canvas-wrap"><div class="pikto-canvas"></div></div></div>
+    <script>(function(d){var js, id="pikto-embed-js", ref=d.getElementsByTagName("script")[0];if (d.getElementById(id)) { return;}js=d.createElement("script"); js.id=id; js.async=true;js.src="https://magic.piktochart.com/assets/embedding/embed.js";ref.parentNode.insertBefore(js, ref);}(document));</script>
