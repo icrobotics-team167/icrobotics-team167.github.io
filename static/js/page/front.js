d518c164d18cd8d67b4dc61e9a5fdf32a89c2140
phantamanta44 <evandalong@gmail.com>
2017-04-14 01:39:53 -0500

    Use match schedule graphic

--- static/js/page/front.js
+++ static/js/page/front.js
@@ -21,30 +21,72 @@ $(document).ready(function() {
         e.css('background-image', 'url({bg})'.supplant({bg: bg}));
     });
     
-    var years = new Date().getFullYear() - 1998;
+    let years = new Date().getFullYear() - 1998;
     $('#year-count').text('{years} Years of Excellence'.supplant({years: years}));
     
-    var video = $('#banner-vid');
-    var updateVideo = function(w) {
+    let video = $('#banner-vid');
+    let updateVideo = function(w) {
         if (w > 768)
             video.get(0).play();
         else
             video.get(0).pause();
     };
     
-    var jWin = $(window);
+    let jWin = $(window);
     updateVideo(jWin.width());
     jWin.resize(function(e) {
         updateVideo($(e.target).width());
     });
 
+    /*
+     * Borrowed from https://github.com/phantamanta44/phantamanta44.github.io
+     * Licensed under the MIT license
+     */
+    let boxScreen = $("#match-schedule-box-shade");
+    let msgBox = $("#match-schedule");
+    let bsTaskId = null;
+
+    let showBoxScreen = function() {
+        if (bsTaskId !== null)
+            window.clearTimeout(bsTaskId);
+        boxScreen.css("display", "block");
+        bsTaskId = window.setTimeout(function() {
+            boxScreen.addClass("visible")
+        }, 1);
+    };
+
+    let hideBoxScreen = function() {
+        if (bsTaskId !== null)
+            window.clearTimeout(bsTaskId);
+        boxScreen.removeClass("visible");
+        bsTaskId = window.setTimeout(function() {
+            boxScreen.css("display", "none");
+        }, 400);
+    };
+
+    let hideBox = function() {
+        hideBoxScreen();
+        msgBox.removeClass("visible");
+    };
+
+    boxScreen.click(hideBox);
+
     if (document.cookie.indexOf("seen-top-extra-bar-lacrosse-2017") === -1) {
-        var teb = $("#top-extra-bar");
+        let teb = $("#top-extra-bar");
         teb.addClass("visible");
+        $("#top-extra-bar-link").click(function() {
+            showBoxScreen();
+            msgBox.addClass("visible");
+        });
         $("#top-extra-bar-close").click(function () {
             teb.removeClass("visible");
             document.cookie = "seen-top-extra-bar-lacrosse-2017=1";
         });
+    }
+
+    if (document.location.hash === "#match-schedule") {
+        showBoxScreen();
+        msgBox.addClass("visible");
     }
     
 });
